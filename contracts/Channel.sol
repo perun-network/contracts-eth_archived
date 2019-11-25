c03bc61688ad6e7055b5f55b8f81c18510dc649e
MariusVanDerWijden <m.vanderwijden@live.de>
2019-11-25 16:01:19 +0100

    :sparkles: Added Perun type definitions

---
+++ contracts/Channel.sol
@@ -0,0 +1,38 @@
+// Copyright (c) 2019 The Perun Authors. All rights reserved.
+// This file is part of go-perun. Use of this source code is governed by a
+// MIT-style license that can be found in the LICENSE file.
+
+pragma solidity ^0.5.11;
+pragma experimental ABIEncoderV2;
+
+library Channel {
+
+    struct Params {
+        uint256 challengeDuration;
+        uint256 nonce;
+        address app;
+        address[] participants;
+    }
+
+    struct State {
+        bytes32 channelID;
+        uint64 version;
+        Allocation outcome;
+        bytes appData;
+        bool isFinal;
+    }
+
+    struct Allocation {
+        address[] assets;
+        uint256[][] balances;
+        SubAlloc[] locked;
+    }
+
+    struct SubAlloc {
+        // ID is the channelID of the subchannel
+        bytes32 ID;
+        // balances holds the total balance of the subchannel of every asset.
+        uint256[] balances;
+    }
+
+}
