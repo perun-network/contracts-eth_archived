96297fbf1d286ec384bb102a19f63b6c54795f0d
MariusVanDerWijden <m.vanderwijden@live.de>
2019-12-07 10:37:36 +0100

    :arrow_up: Updated solc version to 0.5.13

--- contracts/Channel.sol
+++ contracts/Channel.sol
@@ -2,7 +2,7 @@
 // This file is part of go-perun. Use of this source code is governed by a
 // MIT-style license that can be found in the LICENSE file.
 
-pragma solidity ^0.5.11;
+pragma solidity ^0.5.13;
 pragma experimental ABIEncoderV2;
 
 library Channel {
