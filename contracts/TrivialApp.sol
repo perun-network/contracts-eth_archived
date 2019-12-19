danielksan81/contracts-eth

:sparkles: Added Valid transition and trivial app

contracts/TrivialApp.sol
@@ -1,0 +1,22 @@
+// Copyright (c) 2019 The Perun Authors. All rights reserved.
+// This file is part of go-perun. Use of this source code is governed by a
+// MIT-style license that can be found in the LICENSE file.
+
+pragma solidity ^0.5.11;
+pragma experimental ABIEncoderV2;
+
+import "./PerunTypes.sol";
+import "./ValidTransition.sol";
+
+contract TrivialApp is ValidTransitioner {
+
+	function validTransition(
+	    Channel.Params calldata params,
+		Channel.State calldata from,
+		Channel.State calldata to,
+		uint256 moverIdx)
+	external pure returns (bool)
+	{
+	    return true;
+	}
+}

perun-network/contracts-eth_archived

contracts/TrivialApp.sol
@@ -1,0 +1,22 @@
+// Copyright (c) 2019 The Perun Authors. All rights reserved.
+// This file is part of go-perun. Use of this source code is governed by a
+// MIT-style license that can be found in the LICENSE file.
+
+pragma solidity ^0.5.11;
+pragma experimental ABIEncoderV2;
+
+import "./Channel.sol";
+import "./App.sol";
+
+contract TrivialApp is App {
+
+    function validTransition(
+        Channel.Params calldata params,
+        Channel.State calldata from,
+        Channel.State calldata to,
+        uint256 actorIdx)
+    external pure returns (bool)
+    {
+        return true;
+    }
+}