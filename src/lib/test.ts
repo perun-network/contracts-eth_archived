6159c8b21b2d7552ff6644da9eeffefee27d567b
Sebastian Stammler <seb@perun.network>
2020-02-27 21:50:14 +0100

    :recycle: :white_check_mark: split and fix imports
    - split libs between testing and general web3 utilities
    - added functions advanceBlockTime, currentTimestamp and wei2eth
    
    Signed-off-by: Sebastian Stammler <seb@perun.network>

--- src/lib/test.ts
+++ src/lib/test.ts
@@ -5,32 +5,16 @@
 
 /// <reference types="truffle-typings" />
 import Web3 from "web3";
-var web3 = new Web3(Web3.givenProvider || 'http://127.0.0.1:7545/');
-import { promisify } from "util";
-
-
-export async function sign(data: string, account: string) {
-  let sig = await web3.eth.sign(web3.utils.soliditySha3(data), account);
-  // fix wrong v value (add 27)
-  let v = sig.slice(130, 132);
-  return sig.slice(0,130) + (parseInt(v, 16)+27).toString(16);
-}
-
-export function ether(x: number): BN { return web3.utils.toWei(web3.utils.toBN(x), "ether"); }
-
-export const hash = web3.utils.soliditySha3
+declare const web3: Web3;
+import { hash, asyncWeb3Send } from "./web3";
 
 export function sleep(milliseconds: any) {
    return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
-export function asyncWeb3Send(method: string, params: any[], id?: number): Promise<any> {
-  let req: any = { jsonrpc: '2.0', method: method, params: params };
-  if (id != undefined) req.id = id;
-
-  return promisify((callback) => {
-    (web3.currentProvider as any).send(req, callback)
-  })();
+export async function advanceBlockTime(time: number): Promise<any> {
+  await asyncWeb3Send('evm_increaseTime', [time]);
+  return asyncWeb3Send('evm_mine', []);
 }
 
 export function fundingID(channelID: string, participant: string) {
