f05244c4473729dabdd1d9f9b8c8a6b7ff4e0255
Matthias Geihs <matthias@perun.network>
2020-09-07 17:46:16 +0200

    :construction_worker: [CI] enable solidity optimize
    Signed-off-by: Matthias Geihs <matthias@perun.network>

--- truffle-config.js
+++ truffle-config.js
@@ -12,7 +12,13 @@ module.exports = {
 
     compilers: {
         solc: {
-            version: "^0.7.0"
+            version: "^0.7.0",
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 200,
+                },
+            },
         }
     }
 }
