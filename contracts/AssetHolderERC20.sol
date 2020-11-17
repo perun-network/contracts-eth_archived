e07bd1327094dc289d8d903fd88378850ed4061d
Matthias Geihs <matthias@perun.network>
2020-11-17 22:30:01 +0100

    :page_facing_up: Add SPDX license identifier
    Signed-off-by: Matthias Geihs <matthias@perun.network>

--- contracts/AssetHolderERC20.sol
+++ contracts/AssetHolderERC20.sol
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// SPDX-License-Identifier: Apache-2.0
+
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
